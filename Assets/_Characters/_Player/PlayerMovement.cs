c4208c4a87e3b14d76205dbd8fc93b8988cbe5f1
Dave Dong <fudaviddong@gmail.com>
2017-10-08 22:03:37 -0700

    Refactored out third person user control.

---
+++ Assets/_Characters/_Player/PlayerMovement.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityStandardAssets.Characters.ThirdPerson;
+using UnityStandardAssets.CrossPlatformInput;
+
+namespace Game.Characters{
+	public class PlayerMovement : MonoBehaviour {
+		private Transform m_Cam;   
+		private ThirdPersonCharacter m_Character;        
+		private Vector3 m_CamForward;             // The current forward direction of the camera
+        private Vector3 m_Move;
+
+		void Start()
+        {
+            FindMainCamera();
+            m_Character = GetComponent<ThirdPersonCharacter>();
+			
+        }
+
+        private void FindMainCamera()
+        {
+            // get the transform of the main camera
+            if (Camera.main != null)
+            {
+                m_Cam = Camera.main.transform;
+            }
+            else
+            {
+                Debug.LogWarning(
+                    "Warning: no main camera found. Third person character needs a Camera tagged \"MainCamera\", for camera-relative controls.", gameObject);
+                // we use self-relative controls in this case, which probably isn't what the user wants, but hey, we warned them!
+            }
+        }
+
+        private void FixedUpdate()
+        {
+            // read inputs
+            float h = CrossPlatformInputManager.GetAxis("Horizontal");
+            float v = CrossPlatformInputManager.GetAxis("Vertical");
+            bool crouch = Input.GetKey(KeyCode.C);
+
+            // calculate move direction to pass to character
+            if (m_Cam != null)
+            {
+                // calculate camera relative direction to move:
+                m_CamForward = Vector3.Scale(m_Cam.forward, new Vector3(1, 0, 1)).normalized;
+                m_Move = v*m_CamForward + h*m_Cam.right;
+            }
+            else
+            {
+                // we use world-relative directions in the case of no main camera
+                m_Move = v*Vector3.forward + h*Vector3.right;
+            }
+#if !MOBILE_INPUT
+			// walk speed multiplier
+	        if (Input.GetKey(KeyCode.LeftShift)) m_Move *= 0.5f;
+#endif
+
+            // pass all parameters to the character control script
+            m_Character.Move(m_Move, crouch, false);
+
+        }
+	
+	}
+}
+
